2748bb9a6a69351c6469f450f2694bb708b4e0d6
kais manai <kaismanai1975@gmail.com>
2021-09-22 09:59:07 +0100

    Section tranform-css

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,7 +5,7 @@ function Section() {
         <Wrap>
             <ItemText>
                 <h1>Model S</h1>
-                <p>Order On Line For Touchless Delevry</p>
+                <p>order on line for fouchless delevry</p>
             </ItemText>
             <ButtonGroup>
                 <LeftBtn>
@@ -21,13 +21,45 @@ function Section() {
 
 export default Section
 const Wrap=styled.div`
-background-image:url("/images/model-3.jpg");
+background-image:url("/images/model-s.jpg");
 background-size: cover;
 background-position: center;
 background-repeat: no-repeat;
 height:100vh;
+display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 `
 const ItemText=styled.div`
 padding-top:15vh;
 text-aligne:center;
+font-size:12px;
+text-transform: capitalize;
+`
+const ButtonGroup=styled.div`
+width:50vw;
+height:20vh;
+margin: auto;
+margin-bottom: 20px;
+}
+`
+const LeftBtn=styled.button`
+width: 35%;
+height: 20%;
+margin: 10px;
+border-radius: 100px;
+background-color: #393c41;
+color: white;
+cursor:pointer;
+}
+`
+const RightBtn=styled.button`
+width: 35%;
+height: 20%;
+margin: 10px;
+border-radius: 100px;
+background-color:white;
+cursor:pointer;
+
+}
 `
