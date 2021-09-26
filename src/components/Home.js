2996ed1c4756936706725ed6a6b177efc8a51d07
kaisman75 <kaismanai1975gmail.com>
2021-09-26 22:49:49 +0100

    mapping all models

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ function Home() {
     return (
         <Container>
             <Section/>
+            
         </Container>
     )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,28 +1,43 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled from 'styled-components';
+import data from "./carInfo.json";
 function Section() {
+    const carInfo=data;
+    console.log(carInfo);
     return (
-        <Wrap>
-            <ItemText>
-                <h1>Model S</h1>
-                <p>order on line for fouchless delevry</p>
-            </ItemText>
-            <ButtonGroup>
-                <LeftBtn>
-                   Custom Order 
-                </LeftBtn>
-                <RightBtn>
-                  Existing Inventory
-                </RightBtn>
-                <Arrowbtn src='images/down-arrow.svg'
-/>            </ButtonGroup>
-        </Wrap>
-    )
+        <div className="car">
+            {
+                carInfo.map((car) => {
+                    const { title, image } = car;
+                    return (
+                        <Wrap style={{backgroundImage:`url(./${image})`}}>
+                            <ItemText>
+                                <h1>{title}</h1>
+                                <p>order on line for fouchless delevry</p>
+                            </ItemText>
+                            <ButtonGroup>
+                                <LeftBtn>
+                                    Custom Order
+                                </LeftBtn>
+                                <RightBtn>
+                                    Existing Inventory
+                                </RightBtn>
+
+                            </ButtonGroup>
+                            <Arrowbtn src='/images/down-arrow.svg' />
+
+                        </Wrap>
+                    )
+                })
+}
+        </div>
+     
+)
 }
 
 export default Section
-const Wrap=styled.div`
-background-image:url("/images/model-s.jpg");
+const Wrap = styled.div`
+
 background-size: cover;
 background-position: center;
 background-repeat: no-repeat;
@@ -30,24 +45,28 @@ height:100vh;
 display: flex;
     flex-direction: column;
     justify-content: space-between;
+    background-size: cover;
 `
-const ItemText=styled.div`
+const ItemText = styled.div`
 padding-top:15vh;
 text-aligne:center;
 font-size:12px;
 text-transform: capitalize;
 `
-const ButtonGroup=styled.div`
+const ButtonGroup = styled.div`
 width:50vw;
 height:25vh;
 margin: auto;
 margin-bottom: 10px;
 display: flex;
-flex-direction:column;
+
 justify-content: center;
 align-items: center;
+@media (max-width:786px){
+    flex-direction:column;
+}
 `
-const LeftBtn=styled.div`
+const LeftBtn = styled.div`
 width:256px;
 height: 40px;
 margin: 10px;
@@ -59,7 +78,7 @@ display:flex;
 justify-content:center;
 align-items:center;
 `
-const RightBtn=styled.div`
+const RightBtn = styled.div`
 width:256px;
 height: 40px;
 margin: 10px;
@@ -69,10 +88,13 @@ cursor:pointer;
 display:flex;
 justify-content:center;
 align-items:center;
+opacity:0.65;
+color:black;
 `
-const Arrowbtn=styled.img`
+const Arrowbtn = styled.img`
 widtn:20px;
 height:40px;
 cursor:pointer;
+overflow:hidden;
 animation:animateDown infinite 1.5s;
 `
