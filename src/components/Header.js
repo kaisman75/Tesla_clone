0f9662b2efc976366e1a73aa06ea54e02fd6bf1f
kaisman75 <kaismanai1975gmail.com>
2021-10-02 12:37:09 +0100

    TESLA:Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,40 @@
 import React from 'react'
-
+import styled from 'styled-components'
 function Header() {
     return (
-        <div>
-            
-        </div>
+        <Container>
+           <a> 
+            <img src="/images/logo.svg" alt=""/>
+           </a> 
+           <Menu>
+               <a>model s</a>
+               <a>model y</a>
+               <a>model 3</a>
+               <a>model x</a>
+         
+           </Menu>
+        </Container>
     )
 }
 
 export default Header
+
+const Container=styled.div`
+min-height: 20px;
+min-width: 100vw;
+display: -webkit-box;
+display: -webkit-flex;
+display: -ms-flexbox;
+display: flex;
+position: fixed;
+flex-direction: row;
+justify-content: space-around;
+flex-wrap: nowrap;
+`
+const Menu=styled.div`
+display: flex;
+width: 376px;
+justify-content: space-around;
+flex-direction: row;
+
+`
