a64aca79040ea5bd351f5ac47141f7a6ba040e16
kaisman75 <kaismanai1975gmail.com>
2021-10-16 10:13:25 +0100

    implement Redux

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import logo from './logo.svg';
 import Header from './components/Header';
 import Home from './components/Home';
 import './App.css';
+import store from '../src/components/features/store'
+import { carAction } from '../src/components/features/actions/action';
+
+
+
+
 
 function App() {
+  store.dispatch(carAction())  
   return (
     <div className="App">
     {/*--------ce ci est l'entete de la page---------*/}  
     <Header/>
     <Home/>
     </div>
+    
   );
+    
+
+
+  
+ 
+ 
 }
 
 export default App;
